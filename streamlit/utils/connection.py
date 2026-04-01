bd1c66a3c4faa30515d4db6fa26b7f8424d50329
Flavia Ferreira <flavsferr@gmail.com>
2026-04-01 17:59:31 +0200

    fix: add SNOWFLAKE_JWT authenticator to local Snowpark session

--- streamlit/utils/connection.py
+++ streamlit/utils/connection.py
@@ -11,6 +11,7 @@ def get_session():
             "account": os.environ["SNOWFLAKE_ACCOUNT"],
             "user": os.environ["SNOWFLAKE_USER"],
             "private_key_path": os.environ["SNOWFLAKE_PRIVATE_KEY_PATH"],
+            "authenticator": "SNOWFLAKE_JWT",
             "role": os.environ.get("SNOWFLAKE_ROLE", "APEXML_READER"),
             "warehouse": os.environ.get("SNOWFLAKE_WAREHOUSE", "APEXML_WH"),
             "database": os.environ.get("SNOWFLAKE_DATABASE", "APEXML_DB"),
